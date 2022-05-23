edc099dc380742bc77c157b2a05877358da82227
nxhuy1806 <itnxhuy1806@gmail.com>
2022-05-23 14:41:32 +0700

    optimize UI

--- src/routers/Tasks.js
+++ src/routers/Tasks.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import TaskList from "../components/TaskList";
+import Link from '@mui/material/Link'
 import * as API from "../ultis/api"
 
 export default function Tasks() {
@@ -22,6 +22,7 @@ export default function Tasks() {
     useEffect(() => { API.getTask(TaskId, thenGetTask) }, [])
     return (
         <>
+            <h1>{task.content}</h1>
             <TextField
                 id="outlined-multiline-static"
                 label="Description"
@@ -32,6 +33,7 @@ export default function Tasks() {
             />
             <p></p>
             <Button variant="contained" onClick={() => handleUpdate(TaskId, { description: inpDescription })}> Save </Button>
+            <p><Link href={`/#/todos/${task.TodoListId}`}> Back</Link></p>
         </>
     )
 }
